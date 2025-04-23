cc63bfaa34522cd410ab8ccd94ccb65a44ada30c
Shah3949 <shahzebsalahuddin0@gmail.com>
2025-04-23 09:15:52 +0530

    fixed5

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/app/',
+
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
